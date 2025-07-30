f59587293a8fb283c0259971a211c801043aa018
juanoliveira <juanoliveira@mpam.mp.br>
2025-07-30 10:10:40 -0400

    tentando enviar rodar d enovo

--- src/hello_world.ts
+++ src/hello_world.ts
@@ -1,9 +1,8 @@
 const greeting = 'world';
 
-
 export function hello(world: string = greeting): string {
-   if (world == "WebAcademy") {
-       return "Hi WebAcademy!";
-   }
-   return `Hello ${world}!`;
+  if (world == "WebAcademy") {
+    return "Hi WebAcademy!";
+  }
+  return `Hello ${world}!`;
 }
